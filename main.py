247ca7868dd5b9837645ceea0458d4b2c929218c
FK78 <fahad.khan@comparethemarket.com>
2025-03-16 14:14:33 +0000

    split asteroids

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
             for shot in shots:
                 if asteroid.detect_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
         for object in drawable:
