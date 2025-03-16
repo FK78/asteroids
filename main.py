043e391692f8f89e738f5381eeafec1a164f3c87
FK78 <fahad.khan@comparethemarket.com>
2025-03-16 13:53:28 +0000

    add the ability to shoot and detect collisions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import *
 from asteroidfield import *
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()
@@ -11,13 +13,18 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     asteroid_field = AsteroidField()
+    
     player = Player(x, y)
 
 
@@ -26,6 +33,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.detect_collision(player):
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
         for object in drawable:
              object.draw(screen)
